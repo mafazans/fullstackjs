871dda33b165601bd93b0d28371590def758fd51
mafazans <arif.mafazan@gmail.com>
2018-02-08 09:49:39 +0700

    react state

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Header from './Header';
+
+// extends React.Component
+class App extends React.Component {
+  state = {
+  	pageHeader: 'Naming Contests'
+  };
+  render() {
+  	return (
+  	  <div className="App">
+  	  	<Header message={this.state.pageHeader} />
+  	  	<div>
+  	  		...
+  	  	</div>
+  	  </div>
+  	);
+  }
+}
+
+export default App;
