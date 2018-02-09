d518c4de4ca191a705608b97ace9d0b88334e5fe
mafazans <arif.mafazan@gmail.com>
2018-02-09 10:54:50 +0700

    add ContestPreview

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import Header from './Header';
+import ContestPreview from './ContestPreview'
 
 // extends React.Component
 class App extends React.Component {
   state = {
   	pageHeader: 'Naming Contests'
   };
+  componentDidMount(){
+
+  }
+  componentWillMount(){
+
+  }
   render() {
   	return (
   	  <div className="App">
   	  	<Header message={this.state.pageHeader} />
   	  	<div>
-  	  		...
+          {this.props.contests.map(contest =>
+            <ContestPreview {...contest} />
+          )}
   	  	</div>
   	  </div>
   	);
